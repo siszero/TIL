9ec14723b9f86b60af0cfd650224e47e6d341232
eunyoung Hwang <eunyoung0113@naver.com>
2022-01-08 19:20:23 +0900

    15650 조합

--- Java/Algorithm/src/backtracking/Combination.java
+++ Java/Algorithm/src/backtracking/Combination.java
@@ -1,40 +1,39 @@
 package backtracking;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Combination {
-	private static boolean[] visit; // 방문여부
 	private static int[] arr;
 	private static int N, M;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	// N까지의 수 중에서 M개 고르기
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		String s = br.readLine();
+		N = Integer.parseInt(s.split(" ")[0]);
+		M = Integer.parseInt(s.split(" ")[1]);
 
-		arr = new int[N + 1];
-		visit = new boolean[N + 1]; // 방문여부 확인
-		nCr(0); // 처음 0
+		arr = new int[M];
+		nCr(0,1);
+		bw.close();
 	}
 
-	private static void nCr(int cnt) {
-		if (cnt == M) { // 깊이가 M일 때
+	private static void nCr(int depth, int cur) throws IOException {
+		if (depth == M) { // 깊이가 M일 때
 			for (int i = 0; i < M; i++)
-				System.out.print(arr[i] + " ");
-			System.out.println();
+				bw.write(arr[i] + " ");
+			bw.newLine();
+			return;
 		}
-		for (int i = 1; i <= N; i++) {
-			if (!visit[i]) { // 방문한 적이 있는지 확인 (1 1 일때, 2 2 일 때 처럼 중복제외하기 위함)
-				visit[i] = true; // 방문했음을 저장
-				arr[cnt] = i; // 깊이 저장
-				nCr(cnt + 1); // 백트래킹
-				// 중복순열에서 이 부분 추가해주면 중복허락하지 않는 순열
-				for(int j=i+1;j<=N;j++)
-					visit[j] = false; // 방문 저장 초기화
-			}
+		for (int i = cur; i <= N; i++) {
+			arr[depth] = i;
+			nCr(depth + 1, i + 1);
 		}
-
 	}
 
 }
