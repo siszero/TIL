699ec9aee8a9042dfa9c1b4d7c0aea4eb4cf725c
eunyoung Hwang <eunyoung0113@naver.com>
2022-01-09 15:43:47 +0900

    5652 중복조합

--- Java/Algorithm/src/backtracking/Combination_with_repetition.java
+++ Java/Algorithm/src/backtracking/Combination_with_repetition.java
@@ -20,7 +20,7 @@ public class Combination_with_repetition {
 
 		arr = new int[M];
 		nCr(0, 1);
-		bw.close();		// 필수..
+		bw.close();		// 필수...
 	}
 
 	private static void nCr(int depth, int cur) throws IOException {
