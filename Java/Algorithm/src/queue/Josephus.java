69a9866aac3f1b9d495fccc18d6d020d3920bca4
eunyoung Hwang <eunyoung0113@naver.com>
2022-04-26 15:58:23 +0900

    평균구하기

--- Java/Algorithm/src/queue/Josephus.java
+++ Java/Algorithm/src/queue/Josephus.java
@@ -17,6 +17,7 @@ public class Josephus {
 			q.offer(i+1);		// 1부터 넣어주기 때문에
 		
 		System.out.print("<");
+		
 		while(q.size()>1) {		// !q.isEmpty()
 			
 			// k의 배수가 아닌 값 빼내고 그 값을 다시 맨 뒤로
@@ -25,11 +26,10 @@ public class Josephus {
 				q.offer(val);			// 그 값 다시 넣기
 			}
 			System.out.print(q.poll() + ", ");
-	
 		}
+		// 위의 while 문에서 q.size()>0 으로 해줬었는데 그렇게 되면 마지막 값 다음에 , 가 붙어서 나온다.
+		// 그래서 가장 마지막 수는 남겨두고 밑에서 >와 함께 출력하도록 하였다.
 		System.out.print(q.poll() + ">");
-		
-		
 	}
 
 }
