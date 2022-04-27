db86955858d8b7bb59f3029c41ba8790bb59e68f
eunyoung Hwang <eunyoung0113@naver.com>
2022-04-28 00:32:34 +0900

    프로그래머스

--- Java/Algorithm/src/Sorting/BiggestNum.java
+++ Java/Algorithm/src/Sorting/BiggestNum.java
@@ -1,10 +1,35 @@
 package Sorting;
 
+import java.util.*;
+
 public class BiggestNum {
 
+	public static String solution(int[] numbers) {
+		String answer = "";
+        String[] arr = new String[numbers.length];
+        
+        for(int i = 0; i<numbers.length;i++)
+            arr[i] = String.valueOf(numbers[i]);    // 숫자를 문자형으로 변환
+
+        Arrays.sort(arr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        
+        System.out.println((arr[1] + arr[0]).compareTo(arr[0] + arr[1]));
+        
+        // 0 중복 안되게
+        if(arr[0].equals("0"))
+            return "0";
+        
+        for(String i : arr)
+            answer += i;
+        
+        return answer;
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int[] array = { 6, 10, 2 };
+
+		System.out.println(solution(array));
 
 	}
-
 }
