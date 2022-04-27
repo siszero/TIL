ca758fc4241f0330e88fe00ce102292c9ff20d32
eunyoung Hwang <eunyoung0113@naver.com>
2022-04-27 15:38:41 +0900

    투포인터

--- Java/Algorithm/src/twopointer/Boj_1253.java
+++ Java/Algorithm/src/twopointer/Boj_1253.java
@@ -9,31 +9,29 @@ public class Boj_1253 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		int A[] = new int[N + 1];
+		int A[] = new int[N];
 		int count = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 1; i <= N; i++)
+		for (int i = 0; i < N; i++)
 			A[i] = Integer.parseInt(st.nextToken());
 
 		Arrays.sort(A); // 정렬 안 되어있을 것을 생각해서 정렬해주기
 
 		// 비교대상 만들기
-		for (int a = 1; a <= N; a++) {
-			int find = A[a];
+		for (int k = 0; k < N; k++) {
+			int find = A[k];
+			int i = 0;
+			int j = N - 1;
 
-			int i = 1;
-			int j = N;
 			while (i < j) {
-				
 				if (A[i] + A[j] == find) {
-					if(i!=a&&j!=a) {
+					if (i != k && j != k) {
 						count++;
 						break;
-					}
-					else if (i==a)
+					} else if (i == k)
 						i++;
-					else if (j == a)
+					else if (j == k)
 						j--;
 				} else if (A[i] + A[j] < find)
 					i++;
