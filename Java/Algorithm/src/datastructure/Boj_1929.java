0aaf38d7ff6d05f8d5acae47cd8fc88092104dfa
eunyoung Hwang <siszero13@naver.com>
2022-05-03 17:52:58 +0900

    정수론 소수구하기

--- Java/Algorithm/src/datastructure/Boj_1929.java
+++ Java/Algorithm/src/datastructure/Boj_1929.java
@@ -9,32 +9,31 @@ public class Boj_1929 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int M = sc.nextInt();
 		int N = sc.nextInt();
-		int[] arr = new int[N+1];
-		
-		for(int i = 2; i<=N; i++) {
+		int[] arr = new int[N + 1];
+
+		for (int i = 2; i <= N; i++) {
 			arr[i] = i;
 		}
-		
+
 		// N의 제곱근까지 확인 ( N보다 작은 숫자는 항상 N의 제곱근보다 작은 약수를 갖는다.)
-		for(int i =2; i<= Math.sqrt(N); i++) {
-			if(arr[i] == 0) {
+		for (int i = 2; i <= Math.sqrt(N); i++) {
+			if (arr[i] == 0) {
 				continue;
 			}
-			
+
 			// 배수지우기
-			for(int j = i+i; j<=N; j= j+i) {
+			for (int j = i + i; j <= N; j = j + i) {
 				arr[j] = 0;
 			}
 		}
-		
-		for(int i =M; i<=N; i++) {
-			if(arr[i] != 0)
+
+		for (int i = M; i <= N; i++) {
+			if (arr[i] != 0)
 				System.out.println(arr[i]);
 		}
-		
 
 	}
 
